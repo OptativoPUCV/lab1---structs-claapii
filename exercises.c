0eab6e8b11230b83c7e12bd5b6c9057dc4034c86
Your Name <you@example.com>
2024-03-14 23:32:56 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -75,28 +75,37 @@ arreglos en un tercer arreglo también ordenado de menor a mayor.
 void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[])
 {
-  int i, j, k = 0;
-  while (i < size1 && j < size2)
+
+    int i, j, k = 0;
+    while (i < size1 && j < size2)
     {
       if (arr1[i] < arr2[j])
       {
-        result[k++] = arr1[i++];
+        result[k] = arr1[i];
+        i++;
       }
+
       else
       {
-        result[k++] = arr2[j++];
+        result[k] = arr2[j];
+        j++;
       }
+      k++;
     }
-  while (i < size1)
+
+    while (i < size1)
     {
-      result[k++] = arr1[i++];
+      result[k] = arr1[i];
+      i++;
+      k++;
     }
-  while (j < size2)
+    while (j < size2)
     {
-      result[k++] = arr2[j++];
+      result[k] = arr2[j];
+      j++;
+      k++;
     }
-  
-}
+  }
 
 /*
 Ejercicio 5: Comprobación de Ordenación
